7d5e4c46f5ada8f7568a6410db74aae22a4674ab
AMutaa <piusandru@gmail.com>
2018-05-23 17:23:41 -0400

    properties and methods to an object

---
+++ test.js
@@ -0,0 +1,12 @@
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1] = "hello";
+
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Pius";
+myCar.drive = function() {
+  console.log("now driving");
+};
+
+myCar.drive();
