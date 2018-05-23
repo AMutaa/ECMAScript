5ac1cc74952d084ae2c9efee8d1942e8f75adf5a
AMutaa <piusandru@gmail.com>
2018-05-23 10:14:06 -0400

    else If

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
 alert("Hello, World!");
+
+var myAge = 19;
+
+if (myAge > 30) {
+
+ document.write("you are over 30!")
+
+} else if (myAge > 20) {
+
+ document.write("you are over 20!")
+
+} else if (myAge > 10) {
+
+ document.write("you are over 10!")
+
+} else {
+
+ document.write("you are not over 10!")
+
+}
