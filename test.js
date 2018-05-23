eeb53bd3b930012c5755c32060f982539967f0c1
AMutaa <piusandru@gmail.com>
2018-05-23 18:03:58 -0400

    construtor function

--- test.js
+++ test.js
@@ -1,14 +1,18 @@
-var myCar2 = {
-  maxSpeed: 70,
-  driver: "Paul",
-  drive: function(speed, time) {
+var Car = function(maxSpeed, driver) {
+  this.maxSpeed = maxSpeed;
+  this.driver = driver;
+  this.drive = function(speed, time) {
     console.log(speed * time);
-  },
-  logDriver: function() {
+  };
+  this.logDriver = function() {
     console.log("driver name is " + this.driver);
-  }
+  };
 };
 
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+var myCar = new Car(70, "Ninja boy");
+var myCar2 = new Car(10, "Basquiat Boy");
+var myCar3 = new Car(30, "Gerire Boy");
+var myCar4 = new Car(35, "Humpty Dumpty");
+
+myCar.drive(30, 5);
+myCar3.logDriver();
