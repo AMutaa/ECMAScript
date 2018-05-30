20910d1bb6bf0b99267af7b10e5d66ab8852537b
AMutaa <piusandru@gmail.com>
2018-05-30 11:44:48 -0400

    organised files

---
+++ VanillaJS/list.js
@@ -0,0 +1,44 @@
+var todos = ["Buy a New Turtle"];
+
+var input = prompt("What would you like to do?");
+
+while (input !== "quit") {
+  //handle input
+  if (input === "list") {
+    listTodos();
+  } else if (input === "new") {
+    addTodo();
+  } else if (input === "delete") {
+    deletTodo();
+  }
+  //ask again for new input
+  input = prompt("What would you like to do?");
+}
+console.log("OK, YOU CAN QUIT THE APP");
+
+// FUNCTIONS
+
+function listTodos() {
+  console.log("********");
+  todos.forEach(function(todo, i) {
+    console.log(i + ": " + todo);
+  });
+  console.log("********");
+  s;
+}
+function addTodo() {
+  //ask for new todo
+  var newTodo = prompt("Enter new todo");
+  // add to todos array
+  todos.push(newTodo);
+  console.log("Added Todo");
+}
+
+function deleteTodo() {
+  //ask for index of todo to be deleted
+  var index = prompt("Enter index of todo to delete");
+  //delete that todo
+  //splice()
+  todos.splice(index, 1);
+  console.log("Deleted Todo");
+}
