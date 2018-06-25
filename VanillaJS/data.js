bcad984920dc63da502610cd8f4e96c0e2391e88
AMutaa <piusandru@gmail.com>
2018-06-25 12:03:32 -0400

    update

--- VanillaJS/data.js
+++ VanillaJS/data.js
@@ -1,239 +1,229 @@
 const SmsSampleData = {
- response: 'sucess',
- results: [{
-   smsHeader: "Tom's Urban",
-   smsFooter: 'Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix',
-   keyword: 'Beese Churger',
-   connectivityErrorMsg: 'Server error, please try again later',
-   unsupportedErrorMsg: 'Sorry that functionality is not enabled',
-   invalidEmailErrorMsg: 'Sorry,that email address is not recognized',
-   successfulEnromentMsg: 'Congrats! You have Successfully enrolled',
-   alreadyEnrolledMsg: 'That account is already registered',
-   doubleOptInMsg: 'string',
-   regUrl: 'Auto-populated with setting from PXS',
-   enrollmentEmailSubject: 'Enter Subject',
-   enrollmentEmailFrom: 'Enter Email',
-   enrollmentEmailTo: 'Enter to Email',
-   enrollmentEmailReplyTo: 'EnterenrollmentEmailReplyTo',
-   textForEnrollment: 'No free food for you and your friends  at Taco Sub',
-   selectedCardTemplateCode: 1,
-   cardTemplateData: [{
-     cardTemplateCode: 1,
-     cardTemplateLabel: 'Loyalty'
+  response: "sucess",
+  results: [
+    {
+      smsHeader: "Tom's Urban",
+      smsFooter:
+        "Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix",
+      keyword: "Beese Churger",
+      connectivityErrorMsg: "Server error, please try again later",
+      unsupportedErrorMsg: "Sorry that functionality is not enabled",
+      invalidEmailErrorMsg: "Sorry,that email address is not recognized",
+      successfulEnromentMsg: "Congrats! You have Successfully enrolled",
+      alreadyEnrolledMsg: "That account is already registered",
+      doubleOptInMsg: "string",
+      regUrl: "Auto-populated with setting from PXS",
+      enrollmentEmailSubject: "Enter Subject",
+      enrollmentEmailFrom: "Enter Email",
+      enrollmentEmailTo: "Enter to Email",
+      enrollmentEmailReplyTo: "EnterenrollmentEmailReplyTo",
+      textForEnrollment: "No free food for you and your friends  at Taco Sub",
+      selectedCardTemplateCode: 1,
+      cardTemplateData: [
+        {
+          cardTemplateCode: 1,
+          cardTemplateLabel: "Loyalty"
+        },
+        {
+          cardTemplateCode: 2,
+          cardTemplateLabel: "E-gift"
+        },
+        {
+          cardTemplateCode: 3,
+          cardTemplateLabel: "Gift"
+        }
+      ]
     },
     {
-     cardTemplateCode: 2,
-     cardTemplateLabel: 'E-gift'
+      smsHeader: "Tom's Urban",
+      smsFooter:
+        "Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix",
+      keyword: "Quadruple Wrap",
+      connectivityErrorMsg: "Server error, please try again later",
+      unsupportedErrorMsg: "Sorry that functionality is not enabled",
+      invalidEmailErrorMsg: "Sorry,that email address is not recognized",
+      successfulEnromentMsg: "Congrats! You have Successfully enrolled",
+      alreadyEnrolledMsg: "That account is already registered",
+      doubleOptInMsg: "string",
+      regUrl: "Auto-populated with setting from PXS",
+      enrollmentEmailSubject: "Enter Subject",
+      enrollmentEmailFrom: "Enter Email",
+      enrollmentEmailTo: "Enter to Email",
+      enrollmentEmailReplyTo: "EnterenrollmentEmailReplyTo",
+      textForEnrollment: "No free food for you and your friends  at Taco Sub",
+      selectedCardTemplateCode: 1,
+      cardTemplate: [
+        {
+          cardTemplateCode: 1,
+          cardTemplateLabel: "Loyalty"
+        },
+        {
+          cardTemplateCode: 2,
+          cardTemplateLabel: "E-gift"
+        },
+        {
+          cardTemplateCode: 3,
+          cardTemplateLabel: "Gift"
+        }
+      ]
     },
     {
-     cardTemplateCode: 3,
-     cardTemplateLabel: 'Gift'
-    },
-   ],
-  },
-  {
-   smsHeader: "Tom's Urban",
-   smsFooter: 'Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix',
-   keyword: 'Quadruple Wrap',
-   connectivityErrorMsg: 'Server error, please try again later',
-   unsupportedErrorMsg: 'Sorry that functionality is not enabled',
-   invalidEmailErrorMsg: 'Sorry,that email address is not recognized',
-   successfulEnromentMsg: 'Congrats! You have Successfully enrolled',
-   alreadyEnrolledMsg: 'That account is already registered',
-   doubleOptInMsg: 'string',
-   regUrl: 'Auto-populated with setting from PXS',
-   enrollmentEmailSubject: 'Enter Subject',
-   enrollmentEmailFrom: 'Enter Email',
-   enrollmentEmailTo: 'Enter to Email',
-   enrollmentEmailReplyTo: 'EnterenrollmentEmailReplyTo',
-   textForEnrollment: 'No free food for you and your friends  at Taco Sub',
-   selectedCardTemplateCode: 1,
-   cardTemplate: [{
-     cardTemplateCode: 1,
-     cardTemplateLabel: 'Loyalty'
-    },
-    {
-     cardTemplateCode: 2,
-     cardTemplateLabel: 'E-gift'
-    },
-    {
-     cardTemplateCode: 3,
-     cardTemplateLabel: 'Gift'
-    },
-   ],
-  },
-  {
-   smsHeader: 'Express Man',
-   smsFooter: 'Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix',
-   keyword: 'Baconator',
-   connectivityErrorMsg: 'Server error, please try again later',
-   unsupportedErrorMsg: 'Sorry that functionality is not enabled',
-   invalidEmailErrorMsg: 'Sorry,that email address is not recognized',
-   successfulEnromentMsg: 'Congrats! You have Successfully enrolled',
-   alreadyEnrolledMsg: 'That account is already registered',
-   doubleOptInMsg: 'string',
-   regUrl: 'Auto-populated with setting from PXS',
-   enrollmentEmailSubject: 'Enter Subject',
-   enrollmentEmailFrom: 'Enter Email',
-   enrollmentEmailTo: 'Enter to Email',
-   enrollmentEmailReplyTo: 'EnterenrollmentEmailReplyTo',
-   textForEnrollment: 'No free food for you and your friends  at Taco Sub',
-   selectedCardTemplateCode: 1,
-   cardTemplate: [{
-     cardTemplateCode: 1,
-     cardTemplateLabel: 'Loyalty'
+      smsHeader: "Express Man",
+      smsFooter:
+        "Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix",
+      keyword: "Baconator",
+      connectivityErrorMsg: "Server error, please try again later",
+      unsupportedErrorMsg: "Sorry that functionality is not enabled",
+      invalidEmailErrorMsg: "Sorry,that email address is not recognized",
+      successfulEnromentMsg: "Congrats! You have Successfully enrolled",
+      alreadyEnrolledMsg: "That account is already registered",
+      doubleOptInMsg: "string",
+      regUrl: "Auto-populated with setting from PXS",
+      enrollmentEmailSubject: "Enter Subject",
+      enrollmentEmailFrom: "Enter Email",
+      enrollmentEmailTo: "Enter to Email",
+      enrollmentEmailReplyTo: "EnterenrollmentEmailReplyTo",
+      textForEnrollment: "No free food for you and your friends  at Taco Sub",
+      selectedCardTemplateCode: 1,
+      cardTemplate: [
+        {
+          cardTemplateCode: 1,
+          cardTemplateLabel: "Loyalty"
+        },
+        {
+          cardTemplateCode: 2,
+          cardTemplateLabel: "E-gift"
+        },
+        {
+          cardTemplateCode: 3,
+          cardTemplateLabel: "Gift"
+        }
+      ]
     },
     {
-     cardTemplateCode: 2,
-     cardTemplateLabel: 'E-gift'
+      smsHeader: "Tom's Urban",
+      smsFooter:
+        "Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix",
+      keyword: "Cheetos Quesadilla",
+      connectivityErrorMsg: "Server error, please try again later",
+      unsupportedErrorMsg: "Sorry that functionality is not enabled",
+      invalidEmailErrorMsg: "Sorry,that email address is not recognized",
+      successfulEnromentMsg: "Congrats! You have Successfully enrolled",
+      alreadyEnrolledMsg: "That account is already registered",
+      doubleOptInMsg: "string",
+      regUrl: "Auto-populated with setting from PXS",
+      enrollmentEmailSubject: "Enter Subject",
+      enrollmentEmailFrom: "Enter Email",
+      enrollmentEmailTo: "Enter to Email",
+      enrollmentEmailReplyTo: "EnterenrollmentEmailReplyTo",
+      textForEnrollment: "No free food for you and your friends  at Taco Sub",
+      selectedCardTemplateCode: 1,
+      cardTemplate: [
+        {
+          cardTemplateCode: 1,
+          cardTemplateLabel: "Loyalty"
+        },
+        {
+          cardTemplateCode: 2,
+          cardTemplateLabel: "E-gift"
+        },
+        {
+          cardTemplateCode: 3,
+          cardTemplateLabel: "Gift"
+        }
+      ]
     },
     {
-     cardTemplateCode: 3,
-     cardTemplateLabel: 'Gift'
-    },
-   ],
-  },
-  {
-   smsHeader: "Tom's Urban",
-   smsFooter: 'Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix',
-   keyword: 'Cheetos Quesadilla',
-   connectivityErrorMsg: 'Server error, please try again later',
-   unsupportedErrorMsg: 'Sorry that functionality is not enabled',
-   invalidEmailErrorMsg: 'Sorry,that email address is not recognized',
-   successfulEnromentMsg: 'Congrats! You have Successfully enrolled',
-   alreadyEnrolledMsg: 'That account is already registered',
-   doubleOptInMsg: 'string',
-   regUrl: 'Auto-populated with setting from PXS',
-   enrollmentEmailSubject: 'Enter Subject',
-   enrollmentEmailFrom: 'Enter Email',
-   enrollmentEmailTo: 'Enter to Email',
-   enrollmentEmailReplyTo: 'EnterenrollmentEmailReplyTo',
-   textForEnrollment: 'No free food for you and your friends  at Taco Sub',
-   selectedCardTemplateCode: 1,
-   cardTemplate: [{
-     cardTemplateCode: 1,
-     cardTemplateLabel: 'Loyalty'
-    },
-    {
-     cardTemplateCode: 2,
-     cardTemplateLabel: 'E-gift'
-    },
-    {
-     cardTemplateCode: 3,
-     cardTemplateLabel: 'Gift'
-    },
-   ],
-  },
-  {
-   smsHeader: "Tom's Urban",
-   smsFooter: 'Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix',
-   keyword: 'Wrapant',
-   connectivityErrorMsg: 'Server error, please try again later',
-   unsupportedErrorMsg: 'Sorry that functionality is not enabled',
-   invalidEmailErrorMsg: 'Sorry,that email address is not recognized',
-   successfulEnromentMsg: 'Congrats! You have Successfully enrolled',
-   alreadyEnrolledMsg: 'That account is already registered',
-   doubleOptInMsg: 'string',
-   regUrl: 'Auto-populated with setting from PXS',
-   enrollmentEmailSubject: 'Enter Subject',
-   enrollmentEmailFrom: 'Enter Email',
-   enrollmentEmailTo: 'Enter to Email',
-   enrollmentEmailReplyTo: 'EnterenrollmentEmailReplyTo',
-   textForEnrollment: 'No free food for you and your friends  at Taco Sub',
-   selectedCardTemplateCode: 1,
-   cardTemplate: [{
-     cardTemplateCode: 1,
-     cardTemplateLabel: 'Loyalty'
+      smsHeader: "Tom's Urban",
+      smsFooter:
+        "Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix",
+      keyword: "Wrapant",
+      connectivityErrorMsg: "Server error, please try again later",
+      unsupportedErrorMsg: "Sorry that functionality is not enabled",
+      invalidEmailErrorMsg: "Sorry,that email address is not recognized",
+      successfulEnromentMsg: "Congrats! You have Successfully enrolled",
+      alreadyEnrolledMsg: "That account is already registered",
+      doubleOptInMsg: "string",
+      regUrl: "Auto-populated with setting from PXS",
+      enrollmentEmailSubject: "Enter Subject",
+      enrollmentEmailFrom: "Enter Email",
+      enrollmentEmailTo: "Enter to Email",
+      enrollmentEmailReplyTo: "EnterenrollmentEmailReplyTo",
+      textForEnrollment: "No free food for you and your friends  at Taco Sub",
+      selectedCardTemplateCode: 1,
+      cardTemplate: [
+        {
+          cardTemplateCode: 1,
+          cardTemplateLabel: "Loyalty"
+        },
+        {
+          cardTemplateCode: 2,
+          cardTemplateLabel: "E-gift"
+        },
+        {
+          cardTemplateCode: 3,
+          cardTemplateLabel: "Gift"
+        }
+      ]
     },
     {
-     cardTemplateCode: 2,
-     cardTemplateLabel: 'E-gift'
-    },
-    {
-     cardTemplateCode: 3,
-     cardTemplateLabel: 'Gift'
-    },
-   ],
-  },
-  {
-   smsHeader: "Tom's Urban",
-   smsFooter: 'Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix',
-   keyword: 'Run away Burger',
-   connectivityErrorMsg: 'Server error, please try again later',
-   unsupportedErrorMsg: 'Sorry that functionality is not enabled',
-   invalidEmailErrorMsg: 'Sorry,that email address is not recognized',
-   successfulEnromentMsg: 'Congrats! You have Successfully enrolled',
-   alreadyEnrolledMsg: 'That account is already registered',
-   doubleOptInMsg: 'string',
-   regUrl: 'Auto-populated with setting from PXS',
-   enrollmentEmailSubject: 'Enter Subject',
-   enrollmentEmailFrom: 'Enter Email',
-   enrollmentEmailTo: 'Enter to Email',
-   enrollmentEmailReplyTo: 'EnterenrollmentEmailReplyTo',
-   textForEnrollment: 'No free food for you and your friends  at Taco Sub',
-   selectedCardTemplateCode: 1,
-   cardTemplate: [{
-     cardTemplateCode: 1,
-     cardTemplateLabel: 'Loyalty'
-    },
-    {
-     cardTemplateCode: 2,
-     cardTemplateLabel: 'E-gift'
-    },
-    {
-     cardTemplateCode: 3,
-     cardTemplateLabel: 'Gift'
-    },
-   ],
-  },
- ],
+      smsHeader: "Tom's Urban",
+      smsFooter:
+        "Msg & Data rates may apply Txt STOP to stop, HELP for help Pwrd by Paytronix",
+      keyword: "Run away Burger",
+      connectivityErrorMsg: "Server error, please try again later",
+      unsupportedErrorMsg: "Sorry that functionality is not enabled",
+      invalidEmailErrorMsg: "Sorry,that email address is not recognized",
+      successfulEnromentMsg: "Congrats! You have Successfully enrolled",
+      alreadyEnrolledMsg: "That account is already registered",
+      doubleOptInMsg: "string",
+      regUrl: "Auto-populated with setting from PXS",
+      enrollmentEmailSubject: "Enter Subject",
+      enrollmentEmailFrom: "Enter Email",
+      enrollmentEmailTo: "Enter to Email",
+      enrollmentEmailReplyTo: "EnterenrollmentEmailReplyTo",
+      textForEnrollment: "No free food for you and your friends  at Taco Sub",
+      selectedCardTemplateCode: 1,
+      cardTemplate: [
+        {
+          cardTemplateCode: 1,
+          cardTemplateLabel: "Loyalty"
+        },
+        {
+          cardTemplateCode: 2,
+          cardTemplateLabel: "E-gift"
+        },
+        {
+          cardTemplateCode: 3,
+          cardTemplateLabel: "Gift"
+        }
+      ]
+    }
+  ]
 };
 
 // returns an object
 console.log(SmsSampleData);
 
-// returns an array of 6 items
-
-console.log(SmsSampleData.results);
-
-// save all results in a variable and print
-
-var examples = SmsSampleData.results
+var examples = SmsSampleData.results;
 console.log(examples);
-
-
-// print out all arrays
-for (var i = 0; i <= examples.length; i++) {
- console.log(examples);
-}
-
-
-//create an object
-var ObjectTwo = examples[2];
-console.log(ObjectTwo);
-
-// return each key and values in objectTwo
-console.log(Object.keys(ObjectTwo));
-console.log(Object.values(ObjectTwo));
-console.log(ObjectTwo.smsHeader);
-
 
 // attack the array of objects and print out each sms Header
 
 for (var i = 0; i < examples.length; i++) {
- console.log(examples[i].smsHeader);
+  console.log(examples[i].smsHeader);
 }
 
 // attack the array of objects and print out each keyword
 
 for (var i = 0; i < examples.length; i++) {
- console.log(examples[i].keyword);
+  console.log(examples[i].keyword);
 }
 
 // Print out Card Template Labels
 
 var cardTemplates = [];
 for (var i = 0; i < examples.length; i++) {
- cardTemplates.push(examples[i].cardTemplate);
+  cardTemplates.push(examples[i].cardTemplate);
 }
 console.log(cardTemplates);
